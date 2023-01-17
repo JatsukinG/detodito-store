1bb79cf49535adbc06b7002f0e07ff2654bdf3be
sebasarias88 <sebasarias78@gmail.com>
2023-01-17 15:27:11 -0500

    Create header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient1': 'radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%);'
+      },
+    },
   },
   plugins: [],
 }
