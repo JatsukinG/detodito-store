7ec20436bb0812ecd814f8a8a9d85a35cde95bc6
Julian-Trujillo <julian@msbloyalty.com>
2023-01-24 15:01:09 -0500

    Added link tag

--- app/shop/components/ProductCard.tsx
+++ app/shop/components/ProductCard.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import urlFor from '@/lib/urlFor'
 import { useState } from 'react'
 import { FaShoppingCart } from 'react-icons/fa'
+import Link from 'next/link'
 
 const ProductCard = ({product}: any) => {
   const [isMouseOver, setIsMouseOver] = useState<boolean>(false)
@@ -11,28 +12,30 @@ const ProductCard = ({product}: any) => {
   const mouseOut = () => setIsMouseOver(false)
 
   return (
-      <div
-          className="relative w-48 h-64 flex flex-col justify-between"
-          onMouseOver={mouseOver}
-          onMouseOut={mouseOut}
-      >
-        <div className="relative w-48 h-48 overflow-hidden">
-          <Image
-              className='object-cover object-left lg:object-center'
-              src={urlFor(product.image).url()}
-              alt={product.name}
-              fill/>
+      <Link href="/">
+        <div
+            className="relative w-48 h-64 flex flex-col justify-between"
+            onMouseOver={mouseOver}
+            onMouseOut={mouseOut}
+        >
+          <div className="relative w-48 h-48 overflow-hidden">
+            <Image
+                className='object-cover object-left lg:object-center'
+                src={urlFor(product.image).url()}
+                alt={product.name}
+                fill/>
+          </div>
+          <p className="text-center">{product.name.substring(0,15)}</p>
+          <p className={`text-center ${isMouseOver ? 'text-fuchsia-500' : 'text-gray-800'}`}>{product.price}</p>
+          {
+            isMouseOver && (
+                <div className="absolute top-0 left-0 w-48 h-48 bg-black/30 flex justify-center items-center">
+                  <span><FaShoppingCart color="white"/></span>
+                </div>
+              )
+          }
         </div>
-        <p className="text-center">{product.name.substring(0,15)}</p>
-        <p className={`text-center ${isMouseOver ? 'text-fuchsia-500' : 'text-gray-800'}`}>{product.price}</p>
-        {
-          isMouseOver && (
-              <div className="absolute top-0 left-0 w-48 h-48 bg-black/30 flex justify-center items-center">
-                <span><FaShoppingCart color="white"/></span>
-              </div>
-            )
-        }
-      </div>
+      </Link>
 
   )
 }
