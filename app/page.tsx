343d87f5e608821b26cc79f0df1972d5c07b5bb3
Julian-Trujillo <julian@msbloyalty.com>
2023-01-24 10:25:35 -0500

    Added imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { client } from '@/lib/sanity.client'
 import { groq } from 'next-sanity'
 import { previewData } from 'next/headers'
+import Header from '@/app/components/header'
+import Footer from '@/app/components/footer'
 
 const query = groq`
   *[_type == 'prodcut']
@@ -11,11 +13,12 @@ export default async function Home() {
   if (previewData()) {
     return <div>preview mode</div>
   }
+
   return (
       <div className="w-full h-full">
-        <Header/>
+        <Header />
         <div>Not preview</div>
-        <Footer/>
+        <Footer />
       </div>
   )
 }
